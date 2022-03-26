2690eec574934116afce791d130fddf863132755
JessSaavedra <jessicasaavedraval@gmail.com>
2022-03-26 19:15:53 -0300

    Agrega stories

---
+++ src/App.svelte
@@ -0,0 +1,42 @@
+<script>
+	import { Swiper, SwiperSlide } from 'swiper/svelte';
+	import { Autoplay, EffectCube, Navigation, Pagination } from "swiper";
+
+	const pagination = {
+		clickable: true,
+		renderBullet: function (_, className) {
+			return '<span class="' + className + '"></span>';
+		},
+	};
+</script>
+
+<main>
+	<Swiper
+		{pagination}
+		navigation={true}
+		modules={[Autoplay, EffectCube, Navigation, Pagination]}
+		class="stories"
+		spaceBetween={0}
+		slidesPerView={1}
+		effect={"cube"}
+		cubeEffect={{
+			shadow: false
+		}}
+		autoplay={{
+			delay: 5000,
+			disableOnInteraction: false,
+  }}
+	>
+		<SwiperSlide><img class="story" src="https://i.pinimg.com/originals/09/a8/76/09a876a502006380101f3f072421d4b3.jpg" alt="instagram story"/></SwiperSlide>
+		<SwiperSlide><img class="story" src="https://i.pinimg.com/originals/09/a8/76/09a876a502006380101f3f072421d4b3.jpg" alt="instagram story"/></SwiperSlide>
+		<SwiperSlide><img class="story" src="https://i.pinimg.com/originals/09/a8/76/09a876a502006380101f3f072421d4b3.jpg" alt="instagram story"/></SwiperSlide>
+		<SwiperSlide><img class="story" src="https://i.pinimg.com/originals/09/a8/76/09a876a502006380101f3f072421d4b3.jpg" alt="instagram story"/></SwiperSlide>
+	</Swiper>
+</main>
+
+<style>
+	.story {
+		border-radius: 0.5em;
+		width: 100%;
+	}
+</style>
